15e6dcee2104742b725aa225d904d564bd065245
Ryan Atkinson <mail@ryanatkn.com>
2024-02-17 14:02:16 -0700

    wip

--- fuz.config.ts
+++ fuz.config.ts
@@ -4,6 +4,7 @@ const config: Fuz_Config = {
 	deployments: [
 		'https://www.fuz.dev/',
 		'https://template.fuz.dev/',
+		'https://gitops.fuz.dev/',
 		'https://code.fuz.dev/',
 		'https://mastodon.fuz.dev/',
 	],
