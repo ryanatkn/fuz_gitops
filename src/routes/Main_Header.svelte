1ff3b711d873c9cc958167d619c8eddf505a9668
Ryan Atkinson <mail@ryanatkn.com>
2024-04-12 11:24:13 -0600

    wip

--- src/routes/Main_Header.svelte
+++ src/routes/Main_Header.svelte
@@ -1,9 +1,9 @@
 <header class="p_md">
-	<blockquote class="text_align_center pane">
+	<div class="text_align_center pane p_lg bg">
 		<h1 class="p_sm mb_0">fuz_gitops</h1>
 		<div class="icon mb_lg">🪄</div>
 		a tool for managing many repos
-	</blockquote>
+	</div>
 </header>
 
 <style>
