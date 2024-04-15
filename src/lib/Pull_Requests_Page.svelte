a55df5b651105dc43c821e8215b07fa75f44ff9a
Ryan Atkinson <mail@ryanatkn.com>
2024-04-15 11:37:59 -0600

    add moss (#22)

--- src/lib/Pull_Requests_Page.svelte
+++ src/lib/Pull_Requests_Page.svelte
@@ -1,16 +1,19 @@
+<script context="module" lang="ts">
+	// TODO BLOCK is this the new required pattern?
+	export interface Props {
+		deployment: Fetched_Deployment;
+		deployments: Fetched_Deployment[];
+		unfetched_deployments: Unfetched_Deployment[];
+		filter_pull_request?: Filter_Pull_Request | undefined;
+	}
+</script>
+
 <script lang="ts">
 	import Page_Footer from '$lib/Page_Footer.svelte';
 	import Page_Header from '$lib/Page_Header.svelte';
 	import Pull_Requests_Detail from '$lib/Pull_Requests_Detail.svelte';
 	import type {Filter_Pull_Request} from '$lib/github_helpers.js';
 	import type {Fetched_Deployment, Unfetched_Deployment} from '$lib/fetch_deployments.js';
-
-	interface Props {
-		deployment: Fetched_Deployment;
-		deployments: Fetched_Deployment[];
-		unfetched_deployments: Unfetched_Deployment[];
-		filter_pull_request?: Filter_Pull_Request | undefined;
-	}
 
 	const {deployment, deployments, unfetched_deployments, filter_pull_request}: Props = $props();
 </script>
