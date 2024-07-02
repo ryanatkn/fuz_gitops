39166b30bbbb90a5a21ba5f4f600cfb82ef0b8a9
Ryan Atkinson <mail@ryanatkn.com>
2024-07-01 20:44:03 -0600

    wip

--- src/routes/Main_Header.svelte
+++ src/routes/Main_Header.svelte
@@ -1,13 +1,16 @@
+<script lang="ts">
+	import Spider from '@ryanatkn/fuz/Spider.svelte';
+</script>
+
 <header class="p_md">
 	<div class="text_align_center pane p_lg bg">
 		<h1 class="p_sm mb_0">fuz_gitops</h1>
-		<div class="icon mb_lg">🪄</div>
-		a tool for managing many repos
+		<!-- TODO Fuz_Gitops_Logo -->
+		<Spider
+			fill="var(--color_a_5)"
+			size="var(--icon_size_xl2)"
+			label="a friendly blue spider facing you"
+		/>
+		a tool for managing many repos 🪄
 	</div>
 </header>
-
-<style>
-	.icon {
-		font-size: var(--icon_size_lg);
-	}
-</style>
