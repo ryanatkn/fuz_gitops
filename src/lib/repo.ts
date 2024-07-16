7344573ec27dab2102a9fb0c406e2cdc31b04883
Ryan Atkinson <mail@ryanatkn.com>
2024-07-16 09:40:40 -0600

    wip

--- src/lib/repo.ts
+++ src/lib/repo.ts
@@ -1,5 +1,6 @@
 import type {Url} from '@ryanatkn/gro/package_json.js';
-import type {Package_Meta} from '@ryanatkn/gro/package_meta.js';
+import {getContext, setContext} from 'svelte';
+import {parse_package_meta, type Package_Meta} from '@ryanatkn/gro/package_meta.js';
 
 import {Github_Check_Runs_Item, type Github_Pull_Request} from '$lib/github.js';
 
@@ -20,3 +21,45 @@ export interface Unfetched_Repo {
 	check_runs: null;
 	pull_requests: null;
 }
+
+export interface Repos {
+	repo: Fetched_Repo;
+	repos: Fetched_Repo[];
+	unfetched_repos: Unfetched_Repo[];
+}
+
+const KEY = Symbol('Repos');
+
+export const set_repos = (repos: Repos): Repos => setContext(KEY, repos);
+
+export const get_repos = (): Repos => getContext(KEY);
+
+// TODO the types here are hacky, needs rethinking
+export const parse_repos = (maybe_repos: Repo[], homepage_url: string): Repos => {
+	const repos: Fetched_Repo[] = [];
+	const unfetched_repos: Unfetched_Repo[] = [];
+
+	for (const {url, package_json, src_json, check_runs, pull_requests} of maybe_repos) {
+		if (package_json) {
+			repos.push({
+				...parse_package_meta(url, package_json, src_json),
+				check_runs,
+				pull_requests,
+			});
+		} else {
+			unfetched_repos.push({
+				url,
+				package_json: null,
+				src_json: null,
+				check_runs: null,
+				pull_requests: null,
+			});
+		}
+	}
+
+	// We expect to find this because it's sourced from the local package.json
+	const repo = repos.find((d) => d.homepage_url === homepage_url);
+	if (!repo) throw Error(`Cannot find repo with homepage_url: ${homepage_url}`);
+
+	return {repo, repos, unfetched_repos};
+};
