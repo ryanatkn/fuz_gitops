117e0f4e927d58572446180b32d4a844d4fdeaf0
Ryan Atkinson <mail@ryanatkn.com>
2024-07-22 21:11:51 -0600

    upgrade deps

--- src/routes/Main_Header.svelte
+++ src/routes/Main_Header.svelte
@@ -7,7 +7,7 @@
 		<h1 class="p_sm mb_0">fuz_gitops</h1>
 		<!-- TODO Fuz_Gitops_Logo -->
 		<Spider
-			fill="hsl(var(--color_a_5))"
+			fill="var(--color_a_5)"
 			size="var(--icon_size_xl2)"
 			label="a friendly blue spider facing you"
 		/>
