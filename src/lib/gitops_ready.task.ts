6ed050dd650f83005222553d691c0a24bef1e6cf
Ryan Atkinson <mail@ryanatkn.com>
2024-10-25 13:04:51 -0600

    wip

--- src/lib/gitops_ready.task.ts
+++ src/lib/gitops_ready.task.ts
@@ -45,8 +45,7 @@ export const task: Task<Args> = {
 							`Repo ${repo_dir} is not on branch ${repo_config.branch} and the workspace is unclean, blocking switch: ${error_message}`,
 						);
 					}
-					console.log('CHECKING OUT', branch, repo_dir);
-					await git_checkout(branch, {cwd: repo_dir});
+					await git_checkout(repo_config.branch, {cwd: repo_dir});
 				}
 			}),
 		);
