1d09c71e50114757a2488aec23ed5bf93b915cb8
Ryan Atkinson <mail@ryanatkn.com>
2025-11-27 08:37:21 -0500

    wip

--- gitops.config.ts
+++ gitops.config.ts
@@ -3,33 +3,18 @@ import type {CreateGitopsConfig} from './src/lib/gitops_config.js';
 const config: CreateGitopsConfig = () => {
 	return {
 		repos: [
-			{
-				repo_url: 'https://github.com/ryanatkn/zzz',
-				branch: 'main',
-			},
+			'https://github.com/ryanatkn/zzz',
 			'https://github.com/ryanatkn/moss',
 			'https://github.com/ryanatkn/fuz',
 			'https://github.com/ryanatkn/gro',
 			'https://github.com/ryanatkn/belt',
-			{
-				repo_url: 'https://github.com/ryanatkn/fuz_template',
-				branch: 'main',
-			},
-			{
-				repo_url: 'https://github.com/ryanatkn/fuz_blog',
-				branch: 'main',
-			},
-			{
-				repo_url: 'https://github.com/ryanatkn/fuz_mastodon',
-				branch: 'main',
-			},
-			{
-				repo_url: 'https://github.com/ryanatkn/fuz_code',
-				branch: 'main',
-			},
+			'https://github.com/ryanatkn/fuz_template',
+			'https://github.com/ryanatkn/fuz_blog',
+			'https://github.com/ryanatkn/fuz_mastodon',
+			'https://github.com/ryanatkn/fuz_code',
 			{
 				repo_url: 'https://github.com/ryanatkn/fuz_gitops',
-				branch: 'main',
+				branch: 'verbose',
 			},
 		],
 	};
